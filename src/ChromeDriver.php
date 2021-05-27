71ae63d3bb623c5714cbe2c263beaa23f70a9e6d
omerida <oscar@musketeers.me>
2021-05-27 15:33:20 -0400

    Removes a deprecation order since non-optional arguments can not follow optional ones starting in PHP 8.

--- src/ChromeDriver.php
+++ src/ChromeDriver.php
@@ -87,9 +87,12 @@ class ChromeDriver extends CoreDriver
     public function __construct(
         $api_url = 'http://localhost:9222',
         HttpClient $http_client = null,
-        $base_url,
+        $base_url = null,
         $options = []
     ) {
+        if (empty($base_url)) {
+            throw new \InvalidArgumentException("Base URL can not be empty.");
+        }
         if ($http_client == null) {
             $http_client = new HttpClient();
         }
