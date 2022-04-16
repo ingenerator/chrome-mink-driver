36eeaa75c9500708db91d00e22ad946690d8b4f3
Chris Burgess <chris@giantrobot.co.nz>
2022-04-16 16:54:11 +1200

    Extend test coverage
    (cherry picked from commit 57c5110906305466cbd20dd3b094e824f779f8d0)

---
+++ tests/ChromeDriverBasicsTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace DMore\ChromeDriverTests;
+
+use Behat\Mink\Exception\ElementNotFoundException;
+use Behat\Mink\Exception\DriverException;
+use DMore\ChromeDriver\ChromeBrowser as Browser;
+use DMore\ChromeDriver\ChromeDriver;
+use DMore\ChromeDriver\HttpClient;
+use PHPUnit\Framework\TestCase;
+use DMore\ChromeDriverTests\ChromeDriverTestBase;
+
+/**
+ * Note that the majority of driver test coverage is provided via minkphp/driver-testsuite.
+ *
+ * Consider building on coverage there first!
+ */
+class ChromeDriverBasicsTest extends ChromeDriverTestBase
+{
+    /**
+     * Validate test content is matched.
+     *
+     * @throws DriverException
+     */
+    public function testVisitDataUrl()
+    {
+        $html = <<<'HTML'
+<html>
+<body>
+sample text
+</body>
+</html>
+HTML;
+        $url = "data:text/html;charset=utf-8,${html}";
+        $this->driver->visit($url);
+        $this->assertStringContainsString("sample text", $this->driver->getContent());
+    }
+
+    /**
+     * Validate exception when xpath object not found.
+     *
+     * @throws DriverException
+     * @throws \Behat\Mink\Exception\UnsupportedDriverActionException
+     */
+    public function testNotMatchingFormInput()
+    {
+        $this->expectException(ElementNotFoundException::class);
+        $this->expectExceptionMessage('Tag matching xpath "//input[./@name="input2"]" not found.');
+
+        $html = <<<'HTML'
+<html>
+<body>
+<form id="test">
+    <input name="input1" value="foo">
+    <input type="submit">
+</form>
+</body>
+</html>
+HTML;
+
+        $url = "data:text/html;charset=utf-8,${html}";
+        $this->driver->visit($url);
+        $this->driver->setValue('//input[./@name="input2"]', 'bar');
+    }
+
+    /**
+     * Validate getConsoleMessages() retrieves console messages.
+     */
+    public function testConsoleMessages()
+    {
+        $bodyContent = 'some text here';
+        $html = <<<HTML
+<html>
+<body id="content">{$bodyContent}</body>
+</html>
+HTML;
+        $url = "data:text/html;charset=utf-8,${html}";
+        $this->driver->visit($url);
+        $this->driver->evaluateScript("console.log(document.getElementById('content').innerText)");
+        $messages = $this->driver->getConsoleMessages();
+        $this->assertSame($bodyContent, $messages[0]['text']);
+    }
+}
